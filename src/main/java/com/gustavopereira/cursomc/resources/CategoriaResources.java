6f1c9c785e15f3e83a75cc4a4895b262ae65c9b8
Gustavo Pereira <gustavodacostapereira@gmail.com>
2020-03-21 15:20:56 -0400

    Testando classe de dominio

--- src/main/java/com/gustavopereira/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/gustavopereira/cursomc/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.gustavopereira.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gustavopereira.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Teste";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
