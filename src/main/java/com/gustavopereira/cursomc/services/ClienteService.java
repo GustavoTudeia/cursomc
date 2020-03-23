60a40ac4b98f72156ba6e8e2d831868049898253
Gustavo Pereira <gustavodacostapereira@gmail.com>
2020-03-22 22:17:00 -0400

    Endpoint /clientes/{id}

---
+++ src/main/java/com/gustavopereira/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.gustavopereira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavopereira.cursomc.domain.Cliente;
+import com.gustavopereira.cursomc.repositories.ClienteRepository;
+import com.gustavopereira.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscarPorId(Integer id) {
+		Optional<Cliente> categoria = repo.findById(id);
+		return categoria.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}
