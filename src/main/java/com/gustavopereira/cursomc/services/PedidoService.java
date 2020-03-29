3551839e2ed34ce5a36902d0196b4d9b09faa0f4
Gustavo Pereira <gustavodacostapereira@gmail.com>
2020-03-29 15:45:39 -0400

    endpoint/pedidos/{id} disponibilizados

---
+++ src/main/java/com/gustavopereira/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.gustavopereira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavopereira.cursomc.domain.Pedido;
+import com.gustavopereira.cursomc.repositories.PedidoRepository;
+import com.gustavopereira.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscarPorId(Integer id) {
+		Optional<Pedido> categoria = repo.findById(id);
+		return categoria.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
